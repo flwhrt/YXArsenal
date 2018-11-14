c2dee786b8a92949b15f248c08c060344fc4d8f3
flwhrt <flwhrt@163.com>
2018-11-14 09:30:59 +0800

    验证码按钮

--- YXArsenal/YXArsenal.h
+++ YXArsenal/YXArsenal.h
@@ -17,3 +17,5 @@ FOUNDATION_EXPORT const unsigned char YXArsenalVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <YXArsenal/PublicHeader.h>
 
 #import "NSString+YXAdd.h"
+#import "UIColor+YXAdd.h"
+#import "YXVerificationCodeButton.h"
---
+++ YXArsenal/YXVerificationCodeButton.h
@@ -0,0 +1,14 @@
+//
+//  YXb.m
+//  YXArsenal
+//
+//  Created by flwhrt on 2018/11/1.
+//  Copyright © 2018年 flwhrt. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+
+@interface YXVerificationCodeButton : UIButton
+
+@end
